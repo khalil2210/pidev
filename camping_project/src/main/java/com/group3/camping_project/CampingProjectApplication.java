5cc80ea94c2130b3aad40f8110ce0c21e1aaa90a
Safwen Fraj Mrad <frajsafwen.m'rad@esprit.tn>
2023-03-20 02:10:34 +0100

    Initial commit

---
+++ camping_project/src/main/java/com/group3/camping_project/CampingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.group3.camping_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampingProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampingProjectApplication.class, args);
+    }
+
+}
---
+++ camping_project/src/test/java/com/group3/camping_project/CampingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group3.camping_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampingProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
